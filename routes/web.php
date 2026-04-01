de5958fb3bc30708a39d321bfae4f87ea0068cbc
rifa <rifasalsabilazah@gmail.com>
2026-04-01 15:02:17 +0700

    proges admin dashboard template

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::middleware(['isAdmin'])->group(function () {
+    Route::get('/test-admin', function () {
+        return "Admin only";
+    });
+});
+
+Route::get('/admin', function () {
+    return view('admin.dashboard');
 });
